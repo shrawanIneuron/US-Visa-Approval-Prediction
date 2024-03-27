b8d5d05c758c4aa9b19e7e0c6ab7a07f80f9dcec
shrawanIneuron <kumarshrawan9142@gmail.com>
2024-03-27 10:42:59 +0530

    updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -21,6 +21,10 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
